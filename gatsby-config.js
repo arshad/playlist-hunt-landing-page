c968e102133dd8626de7783ed8c11f9a3b1c3f56
Kyle Gill <kylerobertgill@gmail.com>
2020-11-12 23:36:48 -0700

    og image and design tweaks

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,8 @@ require(`dotenv`).config();
 
 module.exports = {
   siteMetadata: {
-    title: "PlaylistHunt",
+    title:
+      "Fullstack Gatsby | Learn to build a fullstack application on Egghead",
     description:
       "Learn how to build PlaylistHunt, a fullstack JAMstack application using Gatsby and other modern tools.",
     siteUrl: process.env.SITE_URL || "http://localhost:8000",
@@ -10,6 +11,7 @@ module.exports = {
       "https://egghead.io/playlists/build-a-full-stack-jamstack-application-with-gatsby-c6f8?af=6xn1w",
   },
   plugins: [
+    "gatsby-plugin-react-helmet",
     "@reflexjs/gatsby-theme-base",
     "@reflexjs/gatsby-theme-post",
     {
